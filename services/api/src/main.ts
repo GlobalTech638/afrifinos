7657a831e6da7bbafe4e330503ef0c7a7ed06583
Veltrra Innovations Labs <josephwambugu216@gmail.com>
2026-09-15 12:46:59 +0300

    Add API bootstrap

---
+++ services/api/src/main.ts
@@ -0,0 +1,9 @@
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module.js";
+
+const port = Number(process.env.PORT ?? 3000);
+
+const app = await NestFactory.create(AppModule);
+app.enableShutdownHooks();
+await app.listen(port);
