9e8b4a8eb3128372032d1ba90b0f3d6892265476
Veltrra Innovations Labs <josephwambugu216@gmail.com>
2026-09-15 13:40:28 +0300

    Run database migrations before API startup

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
+import { createPostgresClient } from "@afrifinos/financial-persistence";
 import { AppModule } from "./app.module.js";
 import { BigIntInterceptor } from "./bigint.interceptor.js";
+import { migrateDatabase } from "./migrations.js";
 
 const port = Number(process.env.PORT ?? 3000);
+const postgresClient = createPostgresClient();
+
+await migrateDatabase(postgresClient);
 
 const app = await NestFactory.create(AppModule);
 app.useGlobalInterceptors(new BigIntInterceptor());
