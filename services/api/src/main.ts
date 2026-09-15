c425df64e7cf807d7d6a521d455b6007bf0f711d
Veltrra Innovations Labs <josephwambugu216@gmail.com>
2026-09-15 12:48:41 +0300

    Register API bigint serialization interceptor

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
+import { BigIntInterceptor } from "./bigint.interceptor.js";
 
 const port = Number(process.env.PORT ?? 3000);
 
 const app = await NestFactory.create(AppModule);
+app.useGlobalInterceptors(new BigIntInterceptor());
 app.enableShutdownHooks();
 await app.listen(port);
